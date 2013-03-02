fd95fa1b3f2e01b8dc7bb1189dda3518a96a12ff
Paul Shoemaker <apshoemaker@gmail.com>
2013-03-02 13:48:06 -0800

    initial commit

--- Clicker/ViewController.h
+++ Clicker/ViewController.h
@@ -7,7 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SMXMLDocument.h"
 
 @interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *numberOfClicks;
+@property (strong, nonatomic) NSMutableData *returnData;
+
+- (void) getTotalClicks;
+- (void) performRequestFor:(NSString *)method;
+- (void) parseResponse:(NSData *)response;
 
 @end
--- Clicker/ViewController.m
+++ Clicker/ViewController.m
@@ -14,16 +14,88 @@
 
 @implementation ViewController
 
+@synthesize numberOfClicks = _numberOfClicks;
+@synthesize returnData = _returnData;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    //add ourselves as an observer to this protocol delegate in case the click count is altered
+    //outside of the app when it is inactive
+    //also takes care of first time gather
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                        selector:@selector(getTotalClicks)
+                                        name:UIApplicationDidBecomeActiveNotification object:nil];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (void) getTotalClicks{
+    [self performRequestFor:@"getTotalClicks"];
+}
+
+- (void) performRequestFor:(NSString *)method{
+    NSString *base = @"http://166.78.136.254:8080/AdkServiceProject/services/Service/";
+    NSString *url = [base stringByAppendingString:method];
+    
+    NSMutableURLRequest *request =  [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]
+                                                            cachePolicy:NSURLRequestUseProtocolCachePolicy
+                                                        timeoutInterval:60.0];
+    
+    [request setHTTPMethod:@"GET"];
+    
+    NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:YES];
+    
+    if(connection){
+        NSLog(@"connection is good");
+        _returnData = [NSMutableData data];
+    } else {
+        NSLog(@"connection is bad");
+    }
+}
+
+- (IBAction)doClick:(UIButton *)sender {
+    [self performRequestFor:@"clickIt"];
+}
+
+
+- (void) parseResponse:(NSData *)response{
+    //parse integer return - same for all requests
+    NSError *error;
+    
+    SMXMLDocument *document = [SMXMLDocument documentWithData:response error:&error];
+    
+    NSString *returnValue = [document.root valueWithPath:@"return"];
+    
+    [_numberOfClicks setText:returnValue];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response{
+    NSLog(@"Received response");
+    [_returnData setLength:0];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data{
+    [_returnData appendData:data];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error{
+    NSLog(@"connection failed to load");
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection{
+    [self parseResponse:_returnData];
+}
+
+- (void)connection:(NSURLConnection *)connection{
+    
 }
 
 @end
